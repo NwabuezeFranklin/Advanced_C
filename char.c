01f5662036f95d9730a0ed84fd67c2a4a44b6ea9
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-06-25 22:11:20 +0100

    Quick recap

---
+++ char.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//No days off
+//Retribution
+
+int main()
+{
+    char d[30], f[30];
+    printf("Enter your first name:");
+    scanf("%s %s", &d, &f);
+    printf("Your  name is %s %s\n", d, f);
+}
